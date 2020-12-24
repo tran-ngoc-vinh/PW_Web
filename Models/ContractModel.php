ca73f6d65aa8a15827a1773ef2ac8eb2ff36966f
tran-ngoc-vinh <tran-ngoc-vinh@morien.com>
2020-12-24 09:25:03 +0900

    int

--- Models/ContractModel.php
+++ Models/ContractModel.php
@@ -34,7 +34,7 @@
         {
             
             $headers = [
-                "Authorization: Bearer yhrwCpik9e7QmS7tn0HXyeLvk6sJnFK0"
+                "Authorization: Bearer c3v84z7lEjbG6G1FKc7OXsqjLNSVKsXu"
             ];
             
             $curl = curl_init( 'https://api.box.com/2.0/folders/128140677013?s=nplent87e2zq3d5pvats7opbs62fa8ah' );
@@ -56,7 +56,7 @@
         {
             
             $headers = [
-                "Authorization: Bearer yhrwCpik9e7QmS7tn0HXyeLvk6sJnFK0"
+                "Authorization: Bearer c3v84z7lEjbG6G1FKc7OXsqjLNSVKsXu"
             ];
             
             $curl = curl_init( 'https://api.box.com/2.0/files/756788264118?s=y3i0siu3tewiv3ky9fb5pseapaf6uwv5' );
