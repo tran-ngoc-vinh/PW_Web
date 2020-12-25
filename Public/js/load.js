47e494b2abacdab54dcfcb67dd50e0cc14971d55
tran-ngoc-vinh <tran-ngoc-vinh@morien.com>
2020-12-25 17:59:00 +0900

    int

--- Public/js/load.js
+++ Public/js/load.js
@@ -1,11 +1,23 @@
-const { Preview } = Box;
-const preview = new Preview();
+// NOTE: This codepen references https://codepen.io/box-platform/pen/xBRLmP for static assets
+
+// How to use this CodePen:
+// 1. Get started with Box Platform and create an application: https://developer.box.com/docs/getting-started-box-platform
+
+// 2. Generate an access token using an SDK or use a developer token from https://app.box.com/developers/console/ -> your application -> configuration in the left sidebar -> Generate Developer Token
+
+// 3. Whitelist 'https://s.codepen.io' in your CORS allowed origins in https://app.box.com/developers/console/ -> your application -> configuration in the left sidebar -> CORS Domains
+
+// 4. Upload a file using a SDK or to your Box account and get a file ID
+
+// 5. Enter your access token below, replacing the existing access token. Replace the file ID with yours from step 4. If you do so, comment out the 'collection' option passed into preview.show().
+
+// NOTE: This codepen uses https://codepen.io/box-platform/pen/xBRLmP for static assets and authentication. The token used is a readonly token accessing public data in a demo enterprise.
+
 var configData = {
 
     ACCESS_TOKEN: "GzKxn6IbnaP2HBEDN98TRsR7nScqrZQ8",
     FILE_ID: '128140677013?s=nplent87e2zq3d5pvats7opbs62fa8ah',
-    FILE_ID_TEXT: '',
-    FILE_ID_DOC: ''
+
 }
 var preview = new Box.Preview();
 preview.show(configData.FILE_ID, configData.ACCESS_TOKEN, {
--- Views/frontend/weblogin/contract.php
+++ Views/frontend/weblogin/contract.php
@@ -110,14 +110,14 @@
                     <?php echo("<pre>"); print_r($post); echo("</pre>"); ?>
                   </div> 
             </form> -->
-            <form action="" class="form-contract-posts" method="POST">
+            <!-- <form action="" class="form-contract-posts" method="POST">
                  <?php foreach($data as $item) : ?>
                 <div> 
                     <p><?php echo $item['FileName'] ?></p>
                     <!-- <img src="<?php echo $item['FileName'] ?>">  -->
                 </div>
                 <?php endforeach; ?>
-               </form>
+               </form> -->
               
           </div>
           <div class="preview-container"></div>
@@ -127,23 +127,3 @@
   
 </body>
 </html>
-<script> 
-
-const { Preview } = Box;
-const preview = new Preview();
-
-var configData = {
-
-    ACCESS_TOKEN: "GzKxn6IbnaP2HBEDN98TRsR7nScqrZQ8",
-    FILE_ID: '128140677013?s=nplent87e2zq3d5pvats7opbs62fa8ah',
-    FILE_ID_TEXT: '',
-    FILE_ID_DOC: ''
-}
-var preview = new Box.Preview();
-preview.show(configData.FILE_ID, configData.ACCESS_TOKEN, {
-    container: '.preview-container',
-    showDownload: true,
-    // Comment out the following if you are using your own access token and file ID
-    collection: [configData.FILE_ID]
-});
-</script>
